b5fc5935eeaeb73806195dff37282fd15d67e967
grandhishyam <n190057@rguktn.ac.in>
2024-09-27 09:48:08 +0530

    NavBar implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-// import reactLogo from './assets/react.svg'
-// import './App.css'
+// import React from 'react'
+import { Grid, GridItem, Show } from "@chakra-ui/react"
+import NavBar from "./components/navBar"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Button colorScheme='blue' size= 'md'>Button</Button>
+    <Grid templateAreas={{
+      base : `"nav" "main"`,
+      lg : ` "nav nav " "aside main"`
+    }}>
+      <GridItem area="nav"  >
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+      <GridItem area="aside"  bg ="coral">
+        aside 
+      </GridItem>
+      </Show>  
+      <GridItem area="main"  bg ="blue">
+        main
+      </GridItem>
+    </Grid>
   )
 }
 
