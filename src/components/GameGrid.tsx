01967149903765ab7385533c8424e1f98725f5f6
grandhishyam <n190057@rguktn.ac.in>
2024-09-27 15:35:46 +0530

    useGames hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,43 +1,17 @@
-import { useEffect, useState } from "react"
-import apiClient from "../services/api-client"
-import { CanceledError } from "axios";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-    id:number;
-    name :string;
-}
-
-interface FetchGamesResponse{
-    count :number;
-    results : Game[];
-}
+import useGames from '../hooks/useGames';
 
 const GameGrid = () => {
-    const [games,setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
- 
-    useEffect(() =>{
-        apiClient
-        .get<FetchGamesResponse>('/games')
-        .then(res => setGames(res.data.results))
-        .catch(err => {
-            if (err in CanceledError)  return;
-            setError(err.message)
-        })
-    } ,[])
-
- 
+    const{games,error}=useGames();
 
   return (
     <> 
       {error && <Text>{error}</Text>}
       <ul>
-      {games.map( game => <li key={game.id}> {game.name}</li>)}
+      {games.map( (game) => <li key={game.id}> {game.name}</li>)}
       </ul>
     </>
     )
-
 }
 
 export default GameGrid
