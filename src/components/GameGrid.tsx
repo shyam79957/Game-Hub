7a5e1cf08dce61882a12573671aab09ea0bbfa83
grandhishyam <n190057@rguktn.ac.in>
2024-09-28 15:27:31 +0530

    Refracting

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,9 +14,9 @@ const GameGrid = ({gameQuery} : Props) => {
     const{data:games,error, isLoading}=useGames(gameQuery);
     const skeletons =[1,2,3,4,5,6];
 
+    if (error) return <Text> {error} </Text>
+    
   return (
-    <> 
-      {error && <Text>{error}</Text>}
       <SimpleGrid columns={{sm:1, md:2, lg:3, xl:4}} spacing={6} padding={5}>
 
         {isLoading && skeletons.map(skeleton => 
@@ -31,7 +31,6 @@ const GameGrid = ({gameQuery} : Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
     )
 }
 
