246870cc199a56569de7a03e998b82999548726d
grandhishyam <n190057@rguktn.ac.in>
2024-09-28 14:08:44 +0530

    claen up GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -15,11 +15,11 @@ const GameCard = ({game} : Props) => {
     <Card >
         <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
-            <Heading fontSize='2xl'> {game.name}</Heading>
-            <HStack justifyContent={'space-between'}>
+            <HStack justifyContent={'space-between'} marginBottom={3}>
               <PlatformIconList key={game.id} platforms={game.parent_platforms.map(p => p.platform)} />
-                <Critic score={game.metacritic}/>
+              <Critic score={game.metacritic}/>
             </HStack>
+            <Heading fontSize='2xl'> {game.name}</Heading>
         </CardBody>
     </Card> 
   )
